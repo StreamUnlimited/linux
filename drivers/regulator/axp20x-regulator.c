8f134e8de2ffc28854b876b7307beae5f473c0ee
Martin Pietryka <martin.pietryka@streamunlimited.com>
2019-08-01 09:06:45 +0200

    regulator: axp20x: add `x-powers,disable-before-poweroff` dts flag
    Signed-off-by: Martin Pietryka <martin.pietryka@streamunlimited.com>
    [rebased for 6.1]
    [rebased for 6.6]
    [rebased for 6.18]
    Signed-off-by: Peter Suti <peter.suti@streamunlimited.com>

--- drivers/regulator/axp20x-regulator.c
+++ drivers/regulator/axp20x-regulator.c
@@ -1623,9 +1623,17 @@ static bool axp20x_is_polyphase_slave(struct axp20x_dev *axp20x, int id)
 	return false;
 }
 
+struct axp20x_pdata {
+	int nregulators;
+	struct regulator_dev **regs;
+	bool *disable_before_poweroff;
+};
+
 static int axp20x_power_off(struct sys_off_data *data)
 {
-	struct axp20x_dev *axp20x = data->cb_data;
+	struct axp20x_pdata *pdata = data->cb_data;
+	struct axp20x_dev *axp20x = rdev_get_drvdata(pdata->regs[0]);
+	int i;
 	unsigned int shutdown_reg;
 
 	switch (axp20x->variant) {
@@ -1638,6 +1646,11 @@ static int axp20x_power_off(struct sys_off_data *data)
 		break;
 	}
 
+	for (i = 0; i < pdata->nregulators; i++) {
+		if (pdata->disable_before_poweroff[i])
+			regulator_disable_regmap(pdata->regs[i]);
+	}
+
 	regmap_write(axp20x->regmap, shutdown_reg, AXP20X_OFF);
 
 	/* Give capacitors etc. time to drain to avoid kernel panic msg. */
@@ -1645,11 +1658,6 @@ static int axp20x_power_off(struct sys_off_data *data)
 
 	return NOTIFY_DONE;
 }
-
-struct axp20x_pdata {
-	int nregulators;
-	struct regulator_dev **regs;
-};
 
 static int axp20x_regulator_probe(struct platform_device *pdev)
 {
@@ -1731,6 +1739,7 @@ static int axp20x_regulator_probe(struct platform_device *pdev)
 	pdata = devm_kzalloc(&pdev->dev, sizeof(*pdata), GFP_KERNEL);
 	pdata->nregulators = nregulators;
 	pdata->regs = devm_kzalloc(&pdev->dev, sizeof(*pdata->regs) * nregulators, GFP_KERNEL);
+	pdata->disable_before_poweroff = devm_kzalloc(&pdev->dev, sizeof(*pdata->disable_before_poweroff) * nregulators, GFP_KERNEL);
 	dev_set_drvdata(&pdev->dev, pdata);
 
 	for (i = 0; i < nregulators; i++) {
@@ -1816,6 +1825,8 @@ static int axp20x_regulator_probe(struct platform_device *pdev)
 					rdev->desc->name);
 		}
 
+		pdata->disable_before_poweroff[i] = of_property_read_bool(rdev->dev.of_node, "x-powers,disable-before-poweroff");
+
 		/*
 		 * Save AXP22X DCDC1 / DCDC5 / AXP15060 ALDO1 regulator names for later.
 		 */
@@ -1853,7 +1864,7 @@ static int axp20x_regulator_probe(struct platform_device *pdev)
 	}
 
 	if (axp20x->variant != AXP288_ID)
-		devm_register_power_off_handler(axp20x->dev, axp20x_power_off, axp20x);
+		devm_register_power_off_handler(axp20x->dev, axp20x_power_off, pdata);
 
 	return 0;
 }
