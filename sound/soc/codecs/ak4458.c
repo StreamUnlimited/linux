ab17a6a689769274106c21b1f4b0a454e6b569b5
Peter Suti <peter.suti@streamunlimited.com>
2022-02-23 15:33:44 +0100

    sound: soc: codecs: ak4458: fix regmap_sync issue on boot
    Sometimes due to timing the regcache_sync fails.
    Add a retry with a bit of a delay because that seems to fix the issue.
    
    Signed-off-by: Peter Suti <peter.suti@streamunlimited.com>
    [rebased to 6.18]
    Signed-off-by: Peter Suti <peter.suti@streamunlimited.com>

--- sound/soc/codecs/ak4458.c
+++ sound/soc/codecs/ak4458.c
@@ -703,9 +703,15 @@ static int ak4458_runtime_resume(struct device *dev)
 	regcache_cache_only(ak4458->regmap, false);
 	regcache_mark_dirty(ak4458->regmap);
 
+	/* There is a tiny chance this fails on boot because of timing reasons, so try again in that case with a bit of a delay */
 	ret = regcache_sync(ak4458->regmap);
-	if (ret)
-		goto err;
+	if (ret) {
+		dev_warn(ak4458->dev, "regcache_sync failed, retrying... \n");
+		usleep_range(5000, 10000);
+		ret = regcache_sync(ak4458->regmap);
+		if (ret)
+			goto err;
+	}
 
 	return 0;
 err:
