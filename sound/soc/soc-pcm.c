d501356a92949c80f3728fd60430448572ab6320
Peter Suti <peter.suti@streamunlimited.com>
2022-07-12 15:14:03 +0200

    ASoC: pcm: fix nullptr when substream is missing
    patch https://www.spinics.net/lists/alsa-devel/msg144612.html
    
    Signed-off-by: Peter Suti <peter.suti@streamunlimited.com>

--- sound/soc/soc-pcm.c
+++ sound/soc/soc-pcm.c
@@ -1300,6 +1300,14 @@ static int dpcm_be_connect(struct snd_soc_pcm_runtime *fe,
 
 	fe_substream = snd_soc_dpcm_get_substream(fe, stream);
 	be_substream = snd_soc_dpcm_get_substream(be, stream);
+	if (!fe_substream) {
+			dev_err(fe->dev, "%s: fe_substream not initialized\n", __func__);
+			return -EINVAL;
+	}
+	if (!be_substream) {
+			dev_err(be->dev, "%s: be_substream not initialized\n", __func__);
+			return -EINVAL;
+	}
 
 	if (!fe_substream->pcm->nonatomic && be_substream->pcm->nonatomic)
 		return snd_soc_ret(be->dev, -EINVAL,
