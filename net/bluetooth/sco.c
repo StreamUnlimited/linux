4c320c03353b6686c813f178f34d5b79010ac422
Pauli Virtanen <pav@iki.fi>
2024-04-07 18:17:19 +0300

    Bluetooth: add experimental BT_POLL_ERRQUEUE socket option
    Add experimental feature that enables a SOL_BLUETOOTH socket option to
    disable POLLERR on non-empty socket error queue.  Add corresponding MGMT
    experimental feature UUID for it.
    
    This is intended to allow applications disable the POLLERR wakeups due
    to TX timestamps, and allow upper layers of the stack to enable and
    consume TX timestamps, without needing to complicate the lower layer
    POLLERR handling which is only interested in real socket errors.
    
    It is a socket option as it enables a deviation from the common net/
    errqueue API.
    
    Signed-off-by: Pauli Virtanen <pav@iki.fi>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    Signed-off-by: Simon Mikuda <simon.mikuda@streamunlimited.com>
    [rebase for 6.18]
    Signed-off-by: Peter Suti <peter.suti@streamunlimited.com>

--- net/bluetooth/sco.c
+++ net/bluetooth/sco.c
@@ -1035,8 +1035,8 @@ static int sco_sock_setsockopt(struct socket *sock, int level, int optname,
 		break;
 
 	default:
-		err = -ENOPROTOOPT;
-		break;
+		release_sock(sk);
+		return bt_sock_setsockopt(sock, level, optname, optval, optlen);
 	}
 
 	release_sock(sk);
@@ -1279,8 +1279,8 @@ static int sco_sock_getsockopt(struct socket *sock, int level, int optname,
 		break;
 
 	default:
-		err = -ENOPROTOOPT;
-		break;
+		release_sock(sk);
+		return bt_sock_getsockopt(sock, level, optname, optval, optlen);
 	}
 
 	release_sock(sk);
