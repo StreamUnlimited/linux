1d8ab291b0151f0703e9e98d92c025d30bf58982
Martin Pietryka <martin.pietryka@streamunlimited.com>
2024-04-16 12:55:46 +0200

    rtkdrivers: rtk-cpufreq: add cpufreq_generic_attr
    This generic attribute field provides the `scaling_available_frequencies`
    attribute in the sysfs.
    
    Signed-off-by: Martin Pietryka <martin.pietryka@streamunlimited.com>

--- drivers/rtkdrivers/cpufreq/rtk-cpufreq.c
+++ drivers/rtkdrivers/cpufreq/rtk-cpufreq.c
@@ -254,6 +254,7 @@ static struct cpufreq_driver rtk_cpufreq_driver = {
 	.verify       = cpufreq_generic_frequency_table_verify,
 	.suspend      = rtk_cpufreq_suspend,
 	.resume       = rtk_cpufreq_resume,
+	.attr         = cpufreq_generic_attr,
 };
 
 static int __init rtk_cpufreq_initcall(void)
