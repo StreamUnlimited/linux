6311f57f7b90cebb71263cec68c24375688f7041
Vadim Fedorenko <vadfed@meta.com>
2024-10-01 05:57:15 -0700

    net_tstamp: add SCM_TS_OPT_ID for RAW sockets
    commit 822b5bc6db55f1c3ea51659c423784ac6919ddd4 upstream.
    
    The last type of sockets which supports SOF_TIMESTAMPING_OPT_ID is RAW
    sockets. To add new option this patch converts all callers (direct and
    indirect) of _sock_tx_timestamp to provide sockcm_cookie instead of
    tsflags. And while here fix __sock_tx_timestamp to receive tsflags as
    __u32 instead of __u16.
    
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Reviewed-by: Jason Xing <kerneljasonxing@gmail.com>
    Signed-off-by: Vadim Fedorenko <vadfed@meta.com>
    Link: https://patch.msgid.link/20241001125716.2832769-3-vadfed@meta.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Simon Mikuda <simon.mikuda@streamunlimited.com>

--- net/packet/af_packet.c
+++ net/packet/af_packet.c
@@ -2105,7 +2105,7 @@ retry:
 	skb->mark = READ_ONCE(sk->sk_mark);
 	skb->tstamp = sockc.transmit_time;
 
-	skb_setup_tx_timestamp(skb, sockc.tsflags);
+	skb_setup_tx_timestamp(skb, &sockc);
 
 	if (unlikely(extra_len == 4))
 		skb->no_fcs = 1;
@@ -2643,7 +2643,7 @@ static int tpacket_fill_skb(struct packet_sock *po, struct sk_buff *skb,
 	skb->priority = READ_ONCE(po->sk.sk_priority);
 	skb->mark = READ_ONCE(po->sk.sk_mark);
 	skb->tstamp = sockc->transmit_time;
-	skb_setup_tx_timestamp(skb, sockc->tsflags);
+	skb_setup_tx_timestamp(skb, sockc);
 	skb_zcopy_set_nouarg(skb, ph.raw);
 
 	skb_reserve(skb, hlen);
@@ -3108,7 +3108,7 @@ static int packet_snd(struct socket *sock, struct msghdr *msg, size_t len)
 		goto out_free;
 	}
 
-	skb_setup_tx_timestamp(skb, sockc.tsflags);
+	skb_setup_tx_timestamp(skb, &sockc);
 
 	if (!vnet_hdr.gso_type && (len > dev->mtu + reserve + extra_len) &&
 	    !packet_extra_vlan_len_allowed(dev, skb)) {
