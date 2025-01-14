StreamUnlimited/linux

commit b91a395d7e06e6f1459d2720cc95050033e84fd2

watchdog: meson: enable watchdog in probe
Signed-off-by: Peter Suti <peter.suti@streamunlimited.com>

drivers/watchdog/meson_gxbb_wdt.c
@@ -212,6 +212,7 @@ static int meson_gxbb_wdt_probe(struct platform_device *pdev)
 
 	writel(ctrl_reg, data->reg_base + GXBB_WDT_CTRL_REG);
 	meson_gxbb_wdt_set_timeout(&data->wdt_dev, data->wdt_dev.timeout);
+	meson_gxbb_wdt_start(&data->wdt_dev);
 
 	dev_info(dev, "Watchdog enabled (timeout=%d sec, nowayout=%d)",
 		 data->wdt_dev.timeout, nowayout);

commit bfb58fcfd1883e4bbe2487dfcfe68d0772064ea0

watchdog: meson: disable keepalive messages sent by kernel after boot
Clear WDOG_HW_RUNNING bit added in commit b91a395d7e06
("watchdog: meson: keep running if already active") because it
would send keepalive to watchdog while kernel is running, but this is not
the behavior we want. Keepalive should only be coming from userspace while
watchdog is active.

Also fix the timeout which is set to the max value, use DEFAULT_TIMEOUT
once the watchdog is started.

Fixes: b91a395d7e06 ("watchdog: meson: enable watchdog in probe")

Signed-off-by: Peter Suti <peter.suti@streamunlimited.com>

drivers/watchdog/meson_gxbb_wdt.c
@@ -211,8 +211,10 @@ static int meson_gxbb_wdt_probe(struct platform_device *pdev)
 			GXBB_WDT_CTRL_CLKDIV_EN;
 
 	writel(ctrl_reg, data->reg_base + GXBB_WDT_CTRL_REG);
-	meson_gxbb_wdt_set_timeout(&data->wdt_dev, data->wdt_dev.timeout);
+	meson_gxbb_wdt_set_timeout(&data->wdt_dev, DEFAULT_TIMEOUT);
 	meson_gxbb_wdt_start(&data->wdt_dev);
+	/* Have to clear this, otherwise kernel sends keepalive */
+	clear_bit(WDOG_HW_RUNNING, &data->wdt_dev.status);
 
 	dev_info(dev, "Watchdog enabled (timeout=%d sec, nowayout=%d)",
 		 data->wdt_dev.timeout, nowayout);