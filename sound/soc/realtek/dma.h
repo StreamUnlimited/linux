526ad260f3272f94a7b2620207435400cb18e7c3
Martin Pietryka <martin.pietryka@streamunlimited.com>
2025-08-19 02:31:09 +0200

    ASoC: realtek: increase DMA buffer size to 768 KiB and period size to 256 KiB
    Realtek gave us a patch where they increase it to 640 KiB, but let's use a
    multiple of 256 for it, should do the job too.
    
    Signed-off-by: Martin Pietryka <martin.pietryka@streamunlimited.com>

--- sound/soc/realtek/dma.h
+++ sound/soc/realtek/dma.h
@@ -8,8 +8,8 @@
 #ifndef __SND_SOC_REALTEK_GDMA_H_
 #define __SND_SOC_REALTEK_GDMA_H_
 
-#define MAX_IDMA_PERIOD (128 * 1024)
-#define MAX_IDMA_BUFFER (160 * 1024)
+#define MAX_IDMA_PERIOD (256 * 1024)
+#define MAX_IDMA_BUFFER (768 * 1024)
 #define DMA_BURST_AGGREGATION 16
 
 //dma fifo infos.
