bf6a8e467a42291b5a19e653f88173085d7d0d38
Martin Pietryka <martin.pietryka@streamunlimited.com>
2025-09-22 13:18:26 +0200

    ASoC: codecs: ameba: correctly limit the ADC rate to 96 kHz [STREAMEIGHT-7717]
    According to the data-sheet the ADC only supports up to 96 kHz.
    
    Signed-off-by: Martin Pietryka <martin.pietryka@streamunlimited.com>

--- sound/soc/codecs/ameba-codec.c
+++ sound/soc/codecs/ameba-codec.c
@@ -1207,9 +1207,9 @@ static struct snd_soc_dai_driver ameba_dai[] = {
 			.stream_name = "AIF2 Capture",
 			.channels_min = 1,
 			.channels_max = 8,
-			.rates = SNDRV_PCM_RATE_8000_192000,
+			.rates = SNDRV_PCM_RATE_8000_96000,
 			.rate_min = 8000,
-			.rate_max = 192000,
+			.rate_max = 96000,
 			.formats = SNDRV_PCM_FMTBIT_S16_LE |
 						SNDRV_PCM_FMTBIT_S24_LE |
 						SNDRV_PCM_FMTBIT_S32_LE,
