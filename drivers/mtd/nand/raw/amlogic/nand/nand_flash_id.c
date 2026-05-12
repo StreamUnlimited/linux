295f3d808c0fdd26de7382341fe1985ff15aa906
Peter Suti <peter.suti@streamunlimited.com>
2026-05-12 08:56:17 +0200

    mtd: nand: raw: amlogic: add support for GD9FU4G8F4DLGI GigaDevice NAND [SGT-139]
    Signed-off-by: Peter Suti <peter.suti@streamunlimited.com>

--- drivers/mtd/nand/raw/amlogic/nand/nand_flash_id.c
+++ drivers/mtd/nand/raw/amlogic/nand/nand_flash_id.c
@@ -11,6 +11,9 @@ struct nand_flash_dev aml_nand_flash_ids[] = {
 	{"TC58NVG2S0H 4G 3.3V 8-bit",
 		{ .id = {0x98, 0xdc, 0x90, 0x26, 0x76, 0x16, 0x08, 0x00} },
 		  SZ_4K, SZ_512, SZ_256K, 0, 8, 256, NAND_ECC_INFO(8, SZ_512) },
+	{"GD9FU4G8F4DLGI 4G 3.3V 8-bit",
+		{ .id = {0xc8, 0xdc, 0x80, 0xa6, 0x63} },
+		  SZ_4K, SZ_512, SZ_256K, 0, 5, 256, NAND_ECC_INFO(8, SZ_512) },
 
 	{NULL,}
 };
